6ee73bc4c78e36e5020a5eb4d19c83ffa069fe93
Andrew Rhoads <andrew.c.rhoads+github@gmail.com>
2015-02-07 20:04:25 -0600

    PEP8

--- checklist/views.py
+++ checklist/views.py
@@ -4,28 +4,30 @@ from django.template import RequestContext, loader
 
 from checklist.models import Checklist, ChecklistSection, ChecklistEntry
 
-def index( request ):
-   checklists = Checklist.objects.order_by( '-createDate' ) 
-   template = loader.get_template( 'checklist/index.html' )
-   context = RequestContext( request, { 'checklists': checklists } )
 
-   return HttpResponse( template.render( context ) )
+def index(request):
+    checklists = Checklist.objects.order_by('-createDate')
+    template = loader.get_template('checklist/index.html')
+    context = RequestContext(request, {'checklists': checklists})
 
-def checklist( request, cid ):
-    context = { }
-    list = Checklist.objects.get( id=cid )
-    sections = ChecklistSection.objects.filter( checklist_id=cid )
-    sections = sections.all( )
+    return HttpResponse(template.render(context))
+
+
+def checklist(request, cid):
+    context = {}
+    list = Checklist.objects.get(id=cid)
+    sections = ChecklistSection.objects.filter(checklist_id=cid)
+    sections = sections.all()
     for section in sections:
-        #FIXME: ordering probably
-        entries = ChecklistEntry.objects.filter( section_id=section.id )
-        section.entries = [ ] 
+        # FIXME: ordering probably
+        entries = ChecklistEntry.objects.filter(section_id=section.id)
+        section.entries = []
         for entry in entries:
-            section.entries.append( entry )
-    
-    context = RequestContext( request, { 'sections': sections } )
-    
-    print( sections )
+            section.entries.append(entry)
 
-    template = loader.get_template( 'checklist/list.html' )
-    return HttpResponse( template.render( context ) )
+    context = RequestContext(request, {'sections': sections})
+
+    print(sections)
+
+    template = loader.get_template('checklist/list.html')
+    return HttpResponse(template.render(context))
