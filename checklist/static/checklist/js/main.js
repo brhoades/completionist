032b4600a013bd549b4a97ba678286d12a2a6398
Billy Rhoades <billyarhoades@gmail.com>
2015-04-12 15:10:30 -0500

    Clicking on caption for checkbox now checks it

--- checklist/static/checklist/js/main.js
+++ checklist/static/checklist/js/main.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {           
-    $('.checkbox').click(function(evt) {                           
+    $('#checkbox').click(function(evt) {                           
             $.ajax(
-                '/check/' + $(evt.target).parent().attr('value'),
+                '/check/' + $(evt.target).attr('value'),
                 { 
                 dataType: 'json',
                 // /check/runid/entryid/
                 success: function(response) { 
-                  $(evt.target).prop('checked',response['state']);
+                  $(evt.target).children('.checkbox').prop('checked',response['state']);
                 },
                 error: function(e, x, r) { 
                   alert("Failed");
-                  alert( $(evt.target).parent().attr('value') );
+                  alert( $(evt.target).attr('value') );
                 }
             }); 
+      $('.checkbox').unbind();
         return false;
 
     }); 
