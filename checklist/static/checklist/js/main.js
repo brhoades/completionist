f88e9ed3aa64a2ec32bf1dbd0abdd79557325f1b
Billy Rhoades <billyarhoades@gmail.com>
2015-04-12 15:26:55 -0500

    Can now use checkbox to check entry

--- checklist/static/checklist/js/main.js
+++ checklist/static/checklist/js/main.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {           
     $('#checkbox').click(function(evt) {                           
             $.ajax(
-                '/check/' + $(evt.target).attr('value'),
+                '/check/' + $(this).attr('value'),
                 { 
                 dataType: 'json',
                 // /check/runid/entryid/
-                success: function(response) { 
-                  $(evt.target).children('.checkbox').prop('checked',response['state']);
+                success: function(response)
+                { 
+                  if(!$(evt.target).is('input'))
+                    evt.target = $(evt.target).children('.checkbox');
+                  $(evt.target).prop('checked',response['state']);
                 },
                 error: function(e, x, r) { 
                   alert("Failed");
-                  alert( $(evt.target).attr('value') );
+                  alert( $(this).attr('value') );
                 }
             }); 
       $('.checkbox').unbind();
