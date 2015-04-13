4e7329151e4ad183aa46dc7869737e32c8beab9f
Billy Rhoades <billyarhoades@gmail.com>
2015-04-12 22:47:17 -0500

    checkboxes now line up with labels

--- checklist/static/checklist/js/main.js
+++ checklist/static/checklist/js/main.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {           
-    $('.checkbox-container').click(function(evt) {                           
+    $('.checkbox').change(function(evt) {                           
             $.ajax(
-                '/check/' + $(this).attr('value'),
+                '/check/' + $(evt.target).parent().attr('value'),
                 { 
                 dataType: 'json',
                 // /check/runid/entryid/
                 success: function(response)
                 { 
-                  if(!$(evt.target).is('input'))
-                    evt.target = $(evt.target).children('.checkbox');
                   $(evt.target).prop('checked',response['state']);
                 },
                 error: function(e, x, r) { 
-                  alert("Failed");
-                  alert( $(this).attr('value') );
+                  alert("Failed" + $(evt.target).parent().attr('value'));
+                  alert( $(evt.target).prop('tagName') );
                 }
             }); 
         return false;
